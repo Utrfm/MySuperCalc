1b57c8f0d936637538bd6e3bd2cf2fa607743685
Utrfm <utrfm25@gmail.com>
2022-12-04 22:46:52 +0300

    2

--- MySuperCalc/ConsoleApp/Program.cs
+++ MySuperCalc/ConsoleApp/Program.cs
@@ -7,45 +7,45 @@ namespace ConsoleCalc // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start");
             //Строка на входе
-            string input = "2.95+(20*2+5**3)*sin45";
+            string input = "2.956+(20.0*2.5+5**3)*sin45";
 
-
+            //стек для чисел и стек для операций
             Stack<Element> numbers = new Stack<Element>();
             Stack<Element> operations = new Stack<Element>();
 
-            string n = "";
-            string s = "";
+            string tmp = "";
 
-            foreach (var ch in input)
+
+            //проходим по строке и собираем стек чисел
+            for (int i = 0; i < input.Length; i++)
             {
-                if (ch >= '0' && ch <= '9' || ch == '.' || ch == ',')
+                if (input[i] >= '0' && input[i] <= '9' || input[i] == '.' || input[i] == ',')
                 {
-                    //Console.WriteLine($"number {ch}");
-                    n += ch;
-                    s += ' ';
-
+                    tmp += input[i];
                 }
                 else
                 {
-                    //Console.WriteLine($"symbol {ch}");
-                    s += ch;
-                    n += ' ';
+                    if (tmp.Length > 0)
+                    {
+                        var e = new Element() { type = (int)ElementType.Number, value = double.Parse(tmp, System.Globalization.CultureInfo.InvariantCulture) };
+                        numbers.Push(e);
+                        tmp = "";
+                    }               
                 }
             }
+            
 
-            Console.WriteLine($"numbers: {n}");
-            Console.WriteLine($"symbols: {s}");
+            //проверка
+            Console.WriteLine($"input string: {input}");
 
-            string[] n2 = n.Split(' ');
-            foreach (var item in n2)
+            foreach (var item in numbers)
             {
-                Console.WriteLine($"{item}");
+                Console.WriteLine(item.value);
             }
 
 
         }
-
-
     }
 }
--- MySuperCalc/ConsoleApp/element.cs
+++ MySuperCalc/ConsoleApp/element.cs
@@ -1,11 +1,30 @@
 using ConsoleCalc;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleCalc
 {
 	public struct Element
 	{
-		public string type; //для чисел type = '0'
-		public double value;
+        public int type;
+        public double value;
+
+		//public Element()
+		//{
+		//	this.Type = type;
+		//	this.value = value;
+		//}
 	}
+
+    public enum ElementType: int
+    {
+        Number,
+        Add,
+        Sub,
+        Div,
+        Multiply,
+        Sin,
+        Cos
+    }
+
 }
